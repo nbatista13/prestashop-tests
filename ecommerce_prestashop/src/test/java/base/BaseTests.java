b9960d9467217938e152a5920456445bce36e265
nbatista13 <nailton.oliveira01@gmail.com>
2020-11-04 15:56:53 -0300

    incluindo teste validação carrinho zerado

--- ecommerce_prestashop/src/test/java/base/BaseTests.java
+++ ecommerce_prestashop/src/test/java/base/BaseTests.java
@@ -9,22 +9,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import pages.HomePage;
 
 public class BaseTests {
-	
+
 	private static WebDriver driver;
 	protected HomePage homePage;
-	
+
 	@BeforeAll
 	public static void inicializar() {
 		System.setProperty("webdriver.chrome.driver", "C:\\webdrivers\\chromedriver\\86\\chromedriver.exe");
 		driver = new ChromeDriver();
 	}
-	
+
 	@BeforeEach
 	public void carregarPaginaInicial() {
 		driver.get("https://marcelodebittencourt.com/demoprestashop/");
 		homePage = new HomePage(driver);
 	}
-	
+
 	@AfterAll
 	public static void finalizar() {
 		driver.quit();
--- ecommerce_prestashop/src/test/java/homepage/HomePageTests.java
+++ ecommerce_prestashop/src/test/java/homepage/HomePageTests.java
@@ -7,11 +7,17 @@ import org.junit.jupiter.api.Test;
 import base.BaseTests;
 
 public class HomePageTests extends BaseTests {
-	
+
 	@Test
 	public void testContarProdutos_oitoProdutosDiferentes() {
 		carregarPaginaInicial();
 		assertThat(homePage.contarProdutos(), is(8));
+	}
+
+	@Test
+	public void testValidarCarrinhoZerado_ZeroItensNoCarrinho() {
+		int produtosNoCarrinho = homePage.obterQuantidadeProdutosNoCarrinho();
+		System.out.println(produtosNoCarrinho);
 	}
 
 }
